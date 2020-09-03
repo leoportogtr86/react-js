de5d31665e14f6ad6e86586f243417180869b4e5
leoporto <leonardoreisporto@gmail.com>
2020-09-03 15:28:37 -0300

    primeiro componente react

---
+++ src/components/Primeiro.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+
+
+
+//criando componente funcional
+
+export default props=>{
+
+    return (
+
+        <div>
+            <h1 className = "text-center text-danger">Hello React</h1>
+        </div>
+    )
+
+
+}
