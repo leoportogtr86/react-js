bd60487f95947b6c6b792d6dcad30ecb4e53c52c
Leo Porto <leoporto_studio@hotmail.com>
2020-09-16 08:35:31 -0300

    renderizacao condicional

--- src/components/Condicional.jsx
+++ src/components/Condicional.jsx
@@ -1,26 +1,33 @@
-import React from 'react'
-import './Condicional.css'
+import React from "react";
+import "./Condicional.css";
 
-export default props =>{
+export default props => {
+  let x = 4;
+  let y = 13;
+  let nome = "Davi";
 
-    let x = 4
-    let y = 13
-    let nome  = 'Davi'
-
-    return (
-
-        <div>
-{/* 
+  return (
+    <div>
+      {/* 
             operador ternario 
             condicao ? true : false */}
 
-            {x > 5 ? <h1>X Maior que 5</h1> : <h1>X Menor que 5</h1>}
+      {x > 5 ? 
+          
+          <h1>X Maior que 5</h1> : 
+          <h1>X Menor que 5</h1>}
 
-            {y%2 == 0 ? <h1>Y é par</h1> : <h1>Y é ímpar</h1> }
+      {y % 2 == 0 ? 
+      
+          <h1>Y é par</h1> : 
+          <h1>Y é ímpar</h1>}
 
-            {nome == 'Leo' ? <h1>Pai</h1> : <h1>Filho</h1>}
+      {nome == "Leo" ? 
+         
+         <h1>Pai</h1> : 
+         <h1>Filho</h1>}
 
-
-        </div>
-    )
-}
+         
+    </div>
+  );
+};
