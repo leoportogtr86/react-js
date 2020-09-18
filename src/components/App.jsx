6b562d72391be862aef04319ca2e303534bb58af
Leo Porto <leoporto_studio@hotmail.com>
2020-09-18 20:42:32 -0300

    renderizacao condicional 2

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,24 +1,25 @@
 import React, { useState } from "react";
 import Menu from "./Menu";
 import Axios from "axios";
-import Condicional from "./Condicional";
+import Condicional2 from "./Condicional2";
 
 //poderiamos carregar a pagina e dar f5 para os numeros iremo mudando automaticamente
 //ou fazer isso via hooks
 
+let x = 11
+
 export default props => {
-  
   return (
     <div>
+      <Condicional2 test = {x%2 == 0}>
+        <h1>Par</h1>
+      </Condicional2>
 
-        
-        <Condicional/>
-      
+      <Condicional2 test = {x%2 != 0}>
 
-      
+        <h1>Ímpar</h1>
 
-
-      
+      </Condicional2>
     </div>
   );
 };
---
+++ src/components/Condicional2.jsx
@@ -0,0 +1,15 @@
+
+
+export default props=>{
+
+
+    if(props.test){
+
+        return props.children
+
+
+    } else {
+
+        return false
+    }
+}
