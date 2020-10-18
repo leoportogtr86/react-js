183ae41dfd8c2fd85140af47b361e86720312846
Leo Porto <leonardoreisporto@gmail.com>
2020-10-18 13:24:51 -0300

    criando componente com svg

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,47 +1,27 @@
-import React from "react";
+import React, { useState } from "react";
+import './App.css'
+import axios from 'axios'
+import Svg from "./Svg";
 
 export default props => {
 
-  let array = ['Leo', 'Davi', 'Liz', 'Chris', 'Juca', 'Jurandir', 'Maria']
 
-  const elementos = array.map((e) => {
-
-    return (
-
-      <div>
-
-        <div className="container-fluid m-3">
-          <div className="row">
-            <div className="col-6">
-              <div className="card">
-                <div className="card-title"><h1 className="text-center text-primary">{e}</h1></div>
-                <div className="card-body">
-                  <p>Corpo do Card.</p>
-                </div>
-              </div>
-
-            </div>
-          </div>
-        </div>
-
-
-      </div>
-    )
-
-  })
   return (
+
+
     <div>
 
       <h1 className="text-center text-primary">Título da Página</h1>
 
       <div className="container">
         <div className="row">
-          <div className="col">
-            {elementos}
+          <div className="col-2">
+
+            <Svg />
+
           </div>
         </div>
       </div>
-
 
 
 
